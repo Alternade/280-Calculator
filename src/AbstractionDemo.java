8229e9dd1428bf25d7ee49b1f84aef8530957c93
Arpit Sahu <aptsahu2@gmail.com>
2023-06-15 21:11:22 +0530

    Added oops part 2

---
+++ src/AbstractionDemo.java
@@ -0,0 +1,26 @@
+public class AbstractionDemo {
+    public static void main(String[] args) {
+        Animal10 a1 = new Dog10();
+        a1.eat();
+        a1.sleep();
+    }
+}
+//abstraction ?: we just want to tell what it does not how it is done
+//abstract class is always have other concrete methods abstraction is not 100 percent
+interface Animal10{
+    public void eat();
+    public void sleep();
+}
+class Dog10 implements Animal10{
+
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Dog is sleeping");
+    }
+}
+//Why interface was introduced? for achieviung 100 percent abstation and to eliminate the diamond problem
